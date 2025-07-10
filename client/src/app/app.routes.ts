af9665cc4993910829390ce00ad68faac999b2d7
HzKueh <kueh4gaming@gmail.com>
2025-07-11 06:11:37 +0800

    Client Routing in Angular

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from '../features/home/home';
+import { UserList } from '../features/users/user-list/user-list';
+import { UserDetail } from '../features/users/user-detail/user-detail';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'users', component: UserList,},
+            { path: 'users/:id', component: UserDetail },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    }
+    ,
+    { path: '**', component: Home },
+];
